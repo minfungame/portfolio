552c7799013d6bfa7c53b23f7dafe7fa4f6d4fe9
minfungame <66049629+minfungame@users.noreply.github.com>
2020-08-10 14:10:37 +0900

    Add files via upload

---
+++ 20922포트폴리오(응프)/지빠귀/asas.js
@@ -0,0 +1,16 @@
+<!DOCTYPE hmtl>
+<html>
+<head>
+<meta charset="utf-8">
+<title>끝말잇기</title>
+<style>
+td{
+  border:1px solid black;
+  width:50px;
+  height: 50px;
+  
+}
+</style>
+</head>
+<body>
+  </html>
